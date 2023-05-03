e0a5b106be9e7d23edbc88f230e4d5d16e0ee2bc
Nikk Forbus <nforbus@pdx.edu>
2023-05-03 13:51:40 -0700

    First commit, partly into the project.

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+    [SerializeField] Transform target; //SerializeField is a Unity function that allows for the serialization of 'private' fields, by default is is just public fields.
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.position;
+    }
+}
