64272ef31e00d93587af39380999dd3c272b0215
Nikk Forbus <nforbus@pdx.edu>
2023-05-17 10:21:16 -0700

    Previous commit didnt get pushed.

--- Assets/Scripts/Mover.cs
+++ Assets/Scripts/Mover.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
@@ -7,9 +7,17 @@ public class Mover : MonoBehaviour
 {
     [SerializeField] Transform target; //SerializeField is a Unity function that allows for the serialization of 'private' fields, by default is is just public fields.
 
+    Ray lastRay;
+
     // Update is called once per frame
     void Update()
     {
+        //GetMouseButtonDown detects button presses, and a value of 0 indicates the left mousebutton
+        if(Input.GetMouseButtonDown(0)) {
+            lastRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        }
+        Debug.DrawRay(lastRay.origin, lastRay.direction * 100);
+
         GetComponent<NavMeshAgent>().destination = target.position;
     }
 }
