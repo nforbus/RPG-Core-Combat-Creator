9a003161b7506bf1ea81dccff7a608a7fb9d60eb
Nikk Forbus <27784291+nforbus@users.noreply.github.com>
2024-08-20 11:08:06 -0700

    * Added Die() to health.cs that triggers when HP is zero and causes death animation * Renamed health to healthPoints in health.cs to make it less ambiguous * Added isDead bool to health.cs

--- Assets/Scripts/Combat/Health.cs
+++ Assets/Scripts/Combat/Health.cs
@@ -4,12 +4,24 @@ namespace RPG.Combat
 {
     public class Health : MonoBehaviour 
     {
-        [SerializeField] float health = 100f;
+        [SerializeField] float healthPoints = 100f;
+        bool isDead = false;
 
         public void TakeDamage(float damage) 
         {
-            health = Mathf.Max(health - damage, 0);
-            print(health);
+            healthPoints = Mathf.Max(healthPoints - damage, 0);
+            //print("healthPoints remaining: " + healthPoints);
+            if(healthPoints == 0) //we can say ==0 instead of <= 0 because the above line guarantees it won't go below 0.
+            {
+                Die();
+            }
+        }
+
+        private void Die()
+        {
+            if(isDead) return;
+            isDead = true;
+            GetComponent<Animator>().SetTrigger("die");
         }
     }
 }
