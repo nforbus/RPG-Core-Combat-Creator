431d9290ea6005b0fadc893f548a8c994118a7f4
Nikk Forbus <27784291+nforbus@users.noreply.github.com>
2024-09-19 14:08:48 -0700

    Broke out the various AIController behaviors into 3 new functions;  AttackBehavior(), SuspicionBehavior(), and GuardBehavior().  Also deleted my .sln files from the directory to clear up the issue of it not seeing extensions.  Unfortunately I don't seem to have Extract Function available any more.  A lesson to never use a designated version of Unity no matter what the course says, just use the latest fucking version when you start, always.

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -1,21 +1,23 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using RPG.Combat;
 using UnityEngine;
 using RPG.Core;
-using RPG.Control;
+using RPG.Movement;
 
 namespace RPG.Control
 {
     public class AIController : MonoBehaviour
     {
         [SerializeField] float chaseDistance = 5f;
+        [SerializeField] float suspicionTime = 5f;
         Fighter fighter;
         Health health;
         Mover mover;
         GameObject player;
 
         Vector3 guardPosition;
+        float timeSinceLastSawPlayer = Mathf.Infinity;
 
         private void Start()
         {
@@ -33,12 +35,31 @@ namespace RPG.Control
 
             if(InAttackRangeOfPlayer() && fighter.CanAttack(player))
             {
-                fighter.Attack(player);
+                timeSinceLastSawPlayer = 0;
+                AttackBehavior();
+            }
+            else if(timeSinceLastSawPlayer < suspicionTime) {
+                //Suspicion State
+                SuspicionBehavior();
             }
             else {
+                GuardBehavior();
+            }
+
+            timeSinceLastSawPlayer = += Time.deltaTime;
+        }
+
+        private void AttackBehavior() {
+            fighter.Attack(player);
+        }
+
+        private void SuspicionBehavior() {
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+        private void GuardBehavior() {
                 //fighter.Cancel();
                 mover.StartMoveAction(guardPosition);
-            }
         }
 
         private bool InAttackRangeOfPlayer()
