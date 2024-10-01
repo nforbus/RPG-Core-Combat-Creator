093438e3941aaf180927c7152a18bac47080df79
Nikk Forbus <27784291+nforbus@users.noreply.github.com>
2024-10-01 14:24:02 -0700

    * PatrolPath.cs - added connecting lines to waypoint paths to see what the actual set path is. * Sandbox Scene - Added additional Waypoints to see more details about new waypoint path default behavior

--- Assets/Scripts/Control/PatrolPath.cs
+++ Assets/Scripts/Control/PatrolPath.cs
@@ -9,11 +9,25 @@ namespace RPG.Control
         const float waypointGizmoRadius = 0.3f;
         
         private void OnDrawGizmos() {
-            for(int i = 0; i < transform.childCount; ++i) {
-                Gizmos.DrawSphere(transform.GetChild(i).position, waypointGizmoRadius);
-
-                
+            for(int i = 0; i < transform.childCount; ++i)
+            {
+                int j = GetNextIndex(i);
+                Gizmos.DrawSphere(GetWaypoint(i), waypointGizmoRadius);
+                Gizmos.DrawLine(GetWaypoint(i), GetWaypoint(j));
             }
+        }
+
+        private int GetNextIndex(int i)
+        {
+            if(i + 1 >= transform.childCount) {
+                return 0;
+            }
+            return i + 1;
+        }
+
+        private Vector3 GetWaypoint(int i)
+        {
+            return transform.GetChild(i).position;
         }
     }
 }
